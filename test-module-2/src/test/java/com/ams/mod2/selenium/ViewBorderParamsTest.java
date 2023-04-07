ea625ec64b8a26279c2824d3ae6581af518357fc
Sergey.Amelchenko <Sergey.Amelchenko@nedra.digital>
2023-04-07 17:31:27 +0300

    Make code refactor - add methods, add conf file.

--- test-module-2/src/test/java/com/ams/mod2/selenium/ViewBorderParamsTest.java
+++ test-module-2/src/test/java/com/ams/mod2/selenium/ViewBorderParamsTest.java
@@ -30,34 +30,62 @@ public class ViewBorderParamsTest {
 
     @Test
     void viewBorderParamsTest() throws InterruptedException {
-        System.out.println("Test Module 2, ViewBorderParamsTest class, inside the package " + this);
+        login();
+        openProject("Февраль 2023");
+        openSettings("Воргенское");
+        openSection();
+        logout();
+    }
 
-        driver.get("http://spektr-frontend-spektr-test.apps.okd.cloud.nedra.digital/home");
-        driver.get("https://keycloak-spektr-keycloak.apps.okd.cloud.nedra.digital/auth/realms/spektr-frontend/protocol/openid-connect/auth?response_type=code&client_id=frontend-web&state=Tn4wVlFaaVlaLmVtQnEzM3h4aWtHTVcwc2pjYmgzbHJ6Y0p1M0tkcm4ycmdV&redirect_uri=http%3A%2F%2Fspektr-frontend-spektr-test.apps.okd.cloud.nedra.digital%2Fhome&scope=openid%20profile%20email%20roles&code_challenge=Gafv5Rj85eDBlmq3opemvTMVxMN12DxxAfrxh0XjJQg&code_challenge_method=S256&nonce=Tn4wVlFaaVlaLmVtQnEzM3h4aWtHTVcwc2pjYmgzbHJ6Y0p1M0tkcm4ycmdV");
-        driver.findElement(By.id("username")).clear();
-        driver.findElement(By.id("username")).sendKeys("testspektr");
-        driver.findElement(By.id("password")).clear();
-        driver.get("http://spektr-frontend-spektr-test.apps.okd.cloud.nedra.digital/home?state=Tn4wVlFaaVlaLmVtQnEzM3h4aWtHTVcwc2pjYmgzbHJ6Y0p1M0tkcm4ycmdV&code=c0879c8c-a47e-403f-a4dd-772b54e07e39.fdacf154-47e5-47ac-8e96-71cf7889e673.4b738cb8-bd31-4209-b409-3bf3f2d56ba7");
-        driver.get("http://spektr-frontend-spektr-test.apps.okd.cloud.nedra.digital/home");
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ошибки'])[1]/following::button[1]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Проекты'])[1]/following::*[name()='svg'][2]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Февраль 2023'])[1]/following::span[1]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Отмена'])[1]/following::button[1]")).click();
-        driver.get("http://spektr-frontend-spektr-test.apps.okd.cloud.nedra.digital/extraction/f6b51fa1-e998-48b6-89a7-36a75b77b27e/graphics?orgId=6&fieldId=all&month=2&year=2023");
-        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/div/div/div[1]")).click();
-        // open valyntoyskoe
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-expertise-conditions/div[1]/div[1]/div[1]/app-objects-tree/div/div[2]/itsk-tree/itsk-tree-host/itsk-tree-item[1]")).click();
-        // open dostrely
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-expertise-conditions/div[1]/div[1]/div[1]/div/div/div[2]/itsk-tabs/div[2]/div/itsk-grid/itsk-grid-body/div/div[7]/div/div/itsk-grid-expand")).click();
-        // open drop down user
-        Thread.sleep(2000);
+    private void logout() throws InterruptedException {
+        // open drop down user logout
         driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-header/div/div[2]/itsk-dropdown/div/div/div/div[1]")).click();
-        //log out
         Thread.sleep(2000);
         driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-header/div/div[2]/itsk-dropdown/div[2]/div/div/span")).click();
         Thread.sleep(2000);
+    }
+
+    private void openSection() throws InterruptedException {
+        // open section
+        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-expertise-conditions/div[1]/div[1]/div[1]/div/div/div[2]/itsk-tabs/div[2]/div/itsk-grid/itsk-grid-body/div/div[7]/div/div/itsk-grid-expand")).click();
+        Thread.sleep(2000);
+    }
+
+    private void openSettings(String field_name_rus) throws InterruptedException {
+        int treeIndex = 0;
+        // open settings
+        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/div/div/div[1]")).click();
+        // open field
+        switch (field_name_rus) {
+            case "Валынтойское":
+                treeIndex = 1;
+                break;
+            case "Воргенское":
+                treeIndex = 2;
+                break;
+            default:
+                treeIndex = 1;
+        }
+
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-expertise-conditions/div[1]/div[1]/div[1]/app-objects-tree/div/div[2]/itsk-tree/itsk-tree-host/itsk-tree-item[" + treeIndex + "]")).click();
+        Thread.sleep(3000);
+    }
+
+    private void openProject(String monthAndYearRus) {
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ошибки'])[1]/following::button[1]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Проекты'])[1]/following::*[name()='svg'][2]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='" + monthAndYearRus +"'])[1]/following::span[1]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Отмена'])[1]/following::button[1]")).click();
+    }
+
+    private void login() {
+        driver.get("http://spektr-frontend-spektr-test.apps.okd.cloud.nedra.digital/home");
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("testspektr");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("cgtrnhp,c2022");
+        driver.findElement(By.id("kc-login")).click();
     }
 
     @AfterAll
