f435e98c3bf8554cae3732bd0a0a4e30641df09c
Sergey.Amelchenko <Sergey.Amelchenko@nedra.digital>
2023-04-09 20:40:55 +0300

    Create base class.
    Create class for user.

---
+++ test-module-2/src/test/java/com/ams/mod2/selenium/TestBase.java
@@ -0,0 +1,115 @@
+package com.ams.mod2.selenium;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestBase {
+    protected WebDriver driver;
+    JavascriptExecutor js;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    protected void logout() throws InterruptedException {
+        // open drop down user logout
+        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-header/div/div[2]/itsk-dropdown/div/div/div/div[1]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-header/div/div[2]/itsk-dropdown/div[2]/div/div/span")).click();
+        Thread.sleep(2000);
+    }
+
+    @BeforeAll
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C://Windows//system32//chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        js = (JavascriptExecutor) driver;
+    }
+
+    protected void openSettings(String field_name_rus) throws InterruptedException {
+        int treeIndex = 0;
+        // open settings
+        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/div/div/div[1]")).click();
+        // open field
+        switch (field_name_rus) {
+            case "Валынтойское":
+                treeIndex = 1;
+                break;
+            case "Воргенское":
+                treeIndex = 2;
+                break;
+            default:
+                treeIndex = 1;
+        }
+
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-expertise-conditions/div[1]/div[1]/div[1]/app-objects-tree/div/div[2]/itsk-tree/itsk-tree-host/itsk-tree-item[" + treeIndex + "]")).click();
+        Thread.sleep(3000);
+    }
+
+    protected void openProject(String monthAndYearRus) {
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ошибки'])[1]/following::button[1]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Проекты'])[1]/following::*[name()='svg'][2]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='" + monthAndYearRus +"'])[1]/following::span[1]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Отмена'])[1]/following::button[1]")).click();
+    }
+
+    protected void login(SpektrUser spektrUser) {
+        driver.get("http://spektr-frontend-spektr-test.apps.okd.cloud.nedra.digital/home");
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys(spektrUser.getLogin());
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys(spektrUser.getPs());
+        driver.findElement(By.id("kc-login")).click();
+    }
+
+    @AfterAll
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
--- test-module-2/src/test/java/com/ams/mod2/selenium/ViewBorderParamsTest.java
+++ test-module-2/src/test/java/com/ams/mod2/selenium/ViewBorderParamsTest.java
@@ -2,47 +2,19 @@ package com.ams.mod2.selenium;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.jupiter.api.Assertions.fail;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class ViewBorderParamsTest {
-    private WebDriver driver;
-    private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
-    JavascriptExecutor js;
-
-
-    @BeforeAll
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C://Windows//system32//chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        //baseUrl = "https://www.google.com/";
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        js = (JavascriptExecutor) driver;
-    }
+public class ViewBorderParamsTest extends TestBase {
 
 
     @Test
     void viewBorderParamsTest() throws InterruptedException {
-        login();
+        login(new SpektrUser());
         openProject("Февраль 2023");
         openSettings("Воргенское");
         openSection();
         logout();
-    }
-
-    private void logout() throws InterruptedException {
-        // open drop down user logout
-        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-header/div/div[2]/itsk-dropdown/div/div/div/div[1]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-header/div/div[2]/itsk-dropdown/div[2]/div/div/span")).click();
-        Thread.sleep(2000);
     }
 
     private void openSection() throws InterruptedException {
@@ -50,86 +22,6 @@ public class ViewBorderParamsTest {
         driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-expertise-conditions/div[1]/div[1]/div[1]/div/div/div[2]/itsk-tabs/div[2]/div/itsk-grid/itsk-grid-body/div/div[7]/div/div/itsk-grid-expand")).click();
         Thread.sleep(2000);
     }
-
-    private void openSettings(String field_name_rus) throws InterruptedException {
-        int treeIndex = 0;
-        // open settings
-        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/div/div/div[1]")).click();
-        // open field
-        switch (field_name_rus) {
-            case "Валынтойское":
-                treeIndex = 1;
-                break;
-            case "Воргенское":
-                treeIndex = 2;
-                break;
-            default:
-                treeIndex = 1;
-        }
-
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("/html/body/app-root/div/app-extraction-project-outlet/app-expertise-conditions/div[1]/div[1]/div[1]/app-objects-tree/div/div[2]/itsk-tree/itsk-tree-host/itsk-tree-item[" + treeIndex + "]")).click();
-        Thread.sleep(3000);
-    }
-
-    private void openProject(String monthAndYearRus) {
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Ошибки'])[1]/following::button[1]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Проекты'])[1]/following::*[name()='svg'][2]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='" + monthAndYearRus +"'])[1]/following::span[1]")).click();
-        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Отмена'])[1]/following::button[1]")).click();
-    }
-
-    private void login() {
-        driver.get("http://spektr-frontend-spektr-test.apps.okd.cloud.nedra.digital/home");
-        driver.findElement(By.id("username")).clear();
-        driver.findElement(By.id("username")).sendKeys("testspektr");
-        driver.findElement(By.id("password")).clear();
-        driver.findElement(By.id("password")).sendKeys("cgtrnhp,c2022");
-        driver.findElement(By.id("kc-login")).click();
-    }
-
-    @AfterAll
-    public void tearDown() throws Exception {
-        driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-            fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
-
 
 
 }
