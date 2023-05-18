c6bcecc8d4d9977f62e6bfefb17508ad16815642
Sergey.Amelchenko <Sergey.Amelchenko@nedra.digital>
2023-05-18 22:16:14 +0300

    Realize 2 types of launch: PER_CLASS/PER_METHOD.

--- test-module-2/src/test/java/com/ams/mod2/selenium/tests/TestBase.java
+++ test-module-2/src/test/java/com/ams/mod2/selenium/tests/TestBase.java
@@ -3,21 +3,38 @@ package com.ams.mod2.selenium.tests;
 import com.ams.mod2.selenium.appmanager.ApplicationManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected final static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
     //ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
     //protected final ApplicationManager app = new ApplicationManager(BrowserType.EDGE);
 
+    protected static String suite;
+
     @BeforeAll
-    public void setUp() throws Exception {
+    public static void setUp() throws Exception {
+    //public void setUp() throws Exception {
+
         app.init();
+        System.out.println("setUp");
     }
 
     @AfterAll
-    public void tearDown() throws Exception {
+    public static void tearDown() throws Exception {
+    //public void tearDown() throws Exception {
         app.stop();
+        System.out.println("tearDown");
+    }
+
+    @BeforeEach
+    public void doSuite(){
+        if (suite == null){
+            suite = "=== field that initialized as a singleton === " + this;
+        }else{
+            System.out.println(suite);
+        }
     }
 
 }
--- test-module-2/src/test/java/com/ams/mod2/selenium/tests/ViewBorderParamsTest.java
+++ test-module-2/src/test/java/com/ams/mod2/selenium/tests/ViewBorderParamsTest.java
@@ -1,10 +1,12 @@
 package com.ams.mod2.selenium.tests;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class ViewBorderParamsTest extends TestBase {
 
     @Test
@@ -14,6 +16,7 @@ public class ViewBorderParamsTest extends TestBase {
         app.getSettingsHelper().openSettingsOfFieldByName("Воргенское");
         app.getSettingsHelper().openSectionDostrely();
         app.getSessionHelper().logout();
+        System.out.println("this = " + this);
     }
 
     @Test
@@ -24,7 +27,19 @@ public class ViewBorderParamsTest extends TestBase {
         app.getSettingsHelper().moveToOptions();
         app.getSettingsHelper().openOptionsDostrely();
         app.getSessionHelper().logout();
+        System.out.println("this = " + this);
     }
+
+    @Test
+    void mockTest1(){
+        System.out.println("mockTest 1 , this = " + this);
+    }
+
+    @Test
+    void mockTest2(){
+        System.out.println("mockTest 2, this = " + this);
+    }
+
 
 
 }
