5b3d1d40252471c07f75fca8a4e7ded17d8cfa5a
Sergey.Amelchenko <Sergey.Amelchenko@nedra.digital>
2025-01-09 16:14:20 +0300

    Add test-new-playwright module
    Add first test

---
+++ test-new-playwright/src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try (Playwright playwright = Playwright.create()) {
+            BrowserType chromium = playwright.chromium();
+            Browser browser = chromium.launch();
+            Page page = browser.newPage();
+            page.navigate("http://dzen.ru");
+            // other actions...
+            browser.close();
+
+            playwright.close();             // remove redundant close
+        }
+
+        //System.out.println( "Hello World!" );
+
+
+    }
+}
